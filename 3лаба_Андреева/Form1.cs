47b9292612caccd30e530f0ca7a5b828148cd40b
OxiD1Zed <skabo1000@mail.ru>
2023-03-31 23:16:44 +0300

    Начальный код

---
+++ 3лаба_Андреева/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Windows.Forms;
+using System.Globalization;
+using System.Diagnostics;
+
+namespace _3лаба_Андреева
+{
+    public partial class Form1 : Form
+    {
+        int heightTextBox2;
+  
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BackColor = Color.AntiqueWhite;
+            heightTextBox2 = textBox2.Height;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.EndsWith("!"))
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                string str2 = Program.Factorial(BigInteger.Parse(textBox1.Text.TrimEnd('!'))).ToString(new NumberFormatInfo());
+                if (str2.Length > 100)
+                {
+                    textBox2.ScrollBars = ScrollBars.Vertical;
+                    textBox2.Size = new Size(textBox2.Width, heightTextBox2 * 2);
+                }
+                textBox2.Text = str2;
+                sw.Stop();
+                label3.Text = sw.ElapsedMilliseconds + " мс";
+            }
+            else
+            {
+                MessageBox.Show("Формат неверный!");
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            const string chars = "1234567890!";
+            for(int i = 0, j = 0; i < textBox1.Text.Trim().Length - j; i++)
+            {
+                if (!chars.Contains(textBox1.Text[i]))
+                {
+                    textBox1.Text = textBox1.Text.Remove(i, 1);
+                    i--;
+                    j++;
+                }
+            }
+        }
+    }
+}
---
+++ 3лаба_Андреева/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace _3лаба_Андреева
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+        }
+
+        public static BigInteger Factorial(BigInteger num)
+        {
+            BigInteger result = new BigInteger(1);
+            for (int i = 1; i <= num; i++)
+                result *= i;
+            return result;
+        }
+
+        //public static long Recursion(long num)
+        //{
+        //    if(num == 0)
+        //    {
+        //        return num;
+        //    }
+        //    else if (num < 0)
+        //    {
+        //        throw new Exception("Факториал отрицательный!");
+        //    }
+        //    return Recursion(num - 1) * num;
+        //}
+    }
+}
